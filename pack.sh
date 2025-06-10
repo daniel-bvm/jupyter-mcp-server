7b34ceecbca59269fc63bcc547dae52fc1391c90
reinSPQR <rein1605@bvm.network>
2025-06-10 18:19:45 +0700

    Update zip file name

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 
-rm -rf vibe_ds.zip
-zip -r vibe_ds.zip jupyter_mcp_server config.json Dockerfile pyproject.toml system_prompt.txt
+rm -rf spark.zip
+zip -r spark.zip jupyter_mcp_server config.json Dockerfile pyproject.toml system_prompt.txt
